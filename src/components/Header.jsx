f46ddba524d997668da3bea373caa4dcde850e58
satyam <satyamb971@gmail.com>
2024-12-28 15:55:45 +0530

    landing page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { Button } from './ui/button'
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+    const navigate = useNavigate()
+    const user = true
+    return (
+        <nav className='py-4 flex justify-between items-center px-4'>
+            <Link to='/'>
+                <img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" alt="google logo" className='h-12' />
+            </Link>
+            <div>
+                {
+                    !user ? (<Button onClic={() => navigate('/auth')} >Login </Button>
+                    ) : (<DropdownMenu>
+                        <DropdownMenuTrigger>
+                            <Avatar >
+                                <AvatarImage src="https://github.com/shadcn.png" />
+                                <AvatarFallback>CN</AvatarFallback>
+                            </Avatar>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent>
+                            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem>Profile</DropdownMenuItem>
+                            <DropdownMenuItem>Billing</DropdownMenuItem>
+                            <DropdownMenuItem>Team</DropdownMenuItem>
+                            <DropdownMenuItem>Subscription</DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                    )
+                }
+            </div>
+        </nav>
+    )
 }
 
 export default Header
